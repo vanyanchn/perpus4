e416aa40913a60ea04437d7a75ad517f411dda62
vanyanchn <vanyanchn@gmail.com>
2025-01-04 13:14:40 +0700

    perpus berubah

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/jungkook', function () {
+    return view('pepus4');
+});
+Route::get('/percobaan', function () {
+    return view('percobaan');
 });
